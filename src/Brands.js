3b437bb7a03456cab30d8bd9405587359bd0a349
Austin <baik.austin@gmail.com>
2022-12-20 16:35:03 -0800

    added outline for Brands.js page

--- src/Brands.js
+++ src/Brands.js
@@ -7,6 +7,21 @@ function Brands(){
 
         <h1>"Brands"</h1>
 
+
+        // API call to db for list of Brands 
+
+
+
+
+        // populate the Brands + Logo 
+
+
+
+        // Form to add Brand 
+        //form details: 
+            //Name 
+            //Url to Brand logo 
+
     );
 
 
